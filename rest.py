4b86778a56c4c058638c71f16ed81af826a020dc
Erick129-ramm <2310629@cesba-queretaro.edu.mx>
2024-10-31 08:26:01 -0600

    primercommit

---
+++ rest.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template
+import mysql.connector as mysqlcon
+
+rest = Flask(__name__)
+
+db = mysqlcon.connect(host="localhost",user="root", password="",database="")
+
+
+@rest.route('/')
+def index():
+   
+    return render_template('index.html')
+
+
+@rest.route('/desayunos')
+def desayunos():
+
+
+    return render_template('desayunos.html')
+
+
+@rest.route('/comida')
+def comida():
+    return render_template('comida.html')
+
+
+@rest.route('/postres')
+def postres():
+    return render_template('postres.html')
+
+
+@rest.route('/bebidas')
+def bebidas():
+    return render_template('bebidas.html')
+@rest.route('/carrito')
+def carrito():
+    return render_template('carrito.html')
+
+
+if  __name__ == '__main__':
+    rest.run()
