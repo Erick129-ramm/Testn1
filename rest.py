ca35a58e29d4dc2ba7068b5efb5bf64c5108e43d
Erick129-ramm <2310629@cesba-queretaro.edu.mx>
2024-10-31 19:23:06 -0600

    primercommit

--- rest.py
+++ rest.py
@@ -3,7 +3,7 @@ import mysql.connector as mysqlcon
 
 rest = Flask(__name__)
 
-db = mysqlcon.connect(host="localhost",user="root", password="",database="")
+db = mysqlcon.connect(host="localhost",user="root", password="",database="restaurante_ing")
 
 
 @rest.route('/')
@@ -11,31 +11,27 @@ def index():
    
     return render_template('index.html')
 
-
 @rest.route('/desayunos')
 def desayunos():
 
 
     return render_template('desayunos.html')
 
-
 @rest.route('/comida')
 def comida():
     return render_template('comida.html')
-
 
 @rest.route('/postres')
 def postres():
     return render_template('postres.html')
 
-
 @rest.route('/bebidas')
 def bebidas():
     return render_template('bebidas.html')
+
 @rest.route('/carrito')
 def carrito():
     return render_template('carrito.html')
-
 
 if  __name__ == '__main__':
     rest.run()
